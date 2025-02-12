3f9bdf2e667f94582f615d605668b4d130617486
Yana Gubskaya <Yana.gubskaja87@gmail.com>
2025-02-12 23:55:27 +0300

    Add new page /about

---
+++ kot_baton_education/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html', context={
+        'who': 'Кот батон',
+    })
+
+
+def about(request):
+    return render(request, 'about.html')
